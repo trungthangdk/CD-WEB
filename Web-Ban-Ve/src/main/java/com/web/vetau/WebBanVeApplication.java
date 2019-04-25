912067abc67ce28e48313e2c132c4af439ca3065
Nguyễn Trung Thắng <trungthangdk@gmail.com>
2019-04-25 23:01:32 +0700

    tạo project web vé tàu
    tạo project web vé tàu

---
+++ Web-Ban-Ve/src/main/java/com/web/vetau/WebBanVeApplication.java
@@ -0,0 +1,13 @@
+package com.web.vetau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanVeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanVeApplication.class, args);
+	}
+
+}
---
+++ Web-Ban-Ve/src/test/java/com/web/vetau/WebBanVeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.web.vetau;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebBanVeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
